9cfcebe9cda064d1a7043f94871255f8c427a2ef
dplastico <42063594+dplastico@users.noreply.github.com>
2021-08-19 15:39:56 -0400

    v2
    lala

---
+++ bot.py
@@ -0,0 +1,63 @@
+import telegram
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+import tracker
+import random
+
+telegram_bot_token = ""
+
+updater = Updater(token=telegram_bot_token, use_context=True)
+dispatcher = updater.dispatcher
+
+
+def hello(update, context):
+    chat_id = update.effective_chat.id
+
+    message = tracker.hello()
+    context.bot.send_message(chat_id=chat_id, text=message)
+
+dispatcher.add_handler(CommandHandler("hello", hello))
+updater.start_polling()
+
+def karoldance(update, context):
+    chat_id = update.effective_chat.id
+    frases = ["lavarte las rendijas", "visitar la porongologa", "masturbarte con cuchillos", "ponete a hervir la tetera", "Son las 420?"]
+    frase = random.choice(frases)
+    message = f"Karol te recomienda {frase}"
+    context.bot.send_message(chat_id=chat_id, text=message)
+
+dispatcher.add_handler(CommandHandler("karoldance", karoldance))
+updater.start_polling()
+
+
+def prices(update, context):
+    chat_id = update.effective_chat.id
+    message = ""
+    response = tracker.prices()
+    #message += response
+    for i in response:
+        message += f"#{response[i]['coin']}\nPrice: ${response[i]['price']}\nDay change: {response[i]['change_day']}\nHour Change: {response[i]['change_hour']}\n"
+        #message = response
+    
+    context.bot.send_message(chat_id=chat_id, text=message)
+
+dispatcher.add_handler(CommandHandler("prices", prices))
+updater.start_polling()
+
+def price(update, context):
+    chat_id = update.effective_chat.id
+    #if len(context.args) > 1:
+    message = tracker.price(str(context.args[0]))
+    context.bot.send_message(chat_id=chat_id, text=message)
+
+dispatcher.add_handler(CommandHandler("price", price))
+updater.start_polling()
+
+
+def commands(update, context):
+    chat_id = update.effective_chat.id
+    message = "Commands:\n/prices\n/hello\n/price <COIN>\n/karoldance\n"
+    context.bot.send_message(chat_id=chat_id, text=message)
+
+dispatcher.add_handler(CommandHandler("help", commands))
+updater.start_polling()
---
+++ tracker.py
@@ -0,0 +1,31 @@
+import requests
+
+
+def hello():
+    return "Hello World!!!"
+
+def prices():
+    coins = ["BTC", "ETH", "XRP", "LTC", "BCH", "ADA", "DOT", "LINK", "BNB", "XLM"]
+
+    crypto_data = requests.get(
+        "https://min-api.cryptocompare.com/data/pricemultifull?fsyms={}&tsyms=USD".format(",".join(coins))).json()["RAW"]
+
+    data = {}
+    for i in crypto_data:
+        data[i] = {
+            "coin": i,
+            "price": crypto_data[i]["USD"]["PRICE"],
+            "change_day": crypto_data[i]["USD"]["CHANGEPCT24HOUR"],
+            "change_hour": crypto_data[i]["USD"]["CHANGEPCTHOUR"]
+        }
+    return data
+
+def price(coin):
+    coins = [coin]
+    crypto_data = requests.get(
+        "https://min-api.cryptocompare.com/data/pricemultifull?fsyms={}&tsyms=USD".format(",".join(coins))).json()["RAW"]
+    return crypto_data[coin]["USD"]["PRICE"]
+
+if __name__ == "__main__":
+        print(get_prices())
+
